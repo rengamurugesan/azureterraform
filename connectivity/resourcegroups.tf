b9ad3e74ea65104826c4a5a3e454a49f97d5113c
Muru <rengamurugesan@hotmail.com>
2025-05-23 19:39:48 +0530

    updated

--- connectivity/resourcegroups.tf
+++ connectivity/resourcegroups.tf
@@ -1,6 +1,11 @@
 resource "azurerm_resource_group" "rg" {
-  provider = azurerm.connectivity
-  for_each = var.regions
-  name     = "rg-${each.key}"
-  location = each.key
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+
+  timeouts {
+    create = lookup(var.timeouts, "create", null)
+    update = lookup(var.timeouts, "update", null)
+    delete = lookup(var.timeouts, "delete", null)
+  }
 }
